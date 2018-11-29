e63d3098154be930afa182b7ac62dee18fbee55a
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-29 17:03:33 +0700

    Create portrait mode notice on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-    <div id="app">
-        Flight Statistics
+    <div id="app" class="bg-blue-darkest min-h-screen font-sans">
+        <ir-screen-md direction="down">
+            <orientation-notice></orientation-notice>
+        </ir-screen-md>
     </div>
 </template>
 
 <script>
+    import OrientationNotice from './components/OrientationNotice'
+
     export default {
+        components: {
+            OrientationNotice
+        }
     }
 </script>
 
 <style>
+    @font-face {
+        font-family: 'Electrolize';
+        src: url('./assets/fonts/Electrolize-Regular.ttf');
+    }
+
     @tailwind preflight;
     @tailwind components;
     @tailwind utilities;
