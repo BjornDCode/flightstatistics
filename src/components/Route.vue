6184a80e3a5b8d96241a47bd779857402f3a54a7
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-30 21:57:59 +0700

    Draw routes as paths instead of lines

--- src/components/Route.vue
+++ src/components/Route.vue
@@ -1,12 +1,14 @@
 <template>
-    <line 
-        :x1="coordinates.x1" 
-        :y1="coordinates.y1" 
-        :x2="coordinates.x2" 
-        :y2="coordinates.y2" 
-        stroke="#DE5A5A" 
-        stroke-width="3" 
-    />
+    <path 
+        v-if="airports" 
+        :d="`
+            M${coordinates.x1},${coordinates.y1} 
+            A${controlPoint.x},${controlPoint.y} 0 0 ${sweep} ${coordinates.x2},${coordinates.y2}
+        `"
+        stroke="#DE5A5A"
+        stroke-width="3"
+        @mouseover="emitData"
+    ></path>
 </template>
 
 <script>
@@ -36,16 +38,43 @@
         computed: {
             coordinates() {
                 if (!this.airports) {
-                    return { x: 0, y: 0 }
+                    return null
                 }
 
                 return {
-                    x1: this.airports.airport1.getAttribute('cx'),
-                    y1: this.airports.airport1.getAttribute('cy'),
-                    x2: this.airports.airport2.getAttribute('cx'),
-                    y2: this.airports.airport2.getAttribute('cy')
+                    x1: parseInt(this.airports.airport1.getAttribute('cx')),
+                    y1: parseInt(this.airports.airport1.getAttribute('cy')),
+                    x2: parseInt(this.airports.airport2.getAttribute('cx')),
+                    y2: parseInt(this.airports.airport2.getAttribute('cy'))
                 }
             },
+
+            sweep() {
+                if (!this.coordinates) {
+                    return 0
+                }
+
+                return (this.coordinates.x1 > this.coordinates.x2) ? 0 : 1
+            },
+
+            controlPoint() {
+                if (!this.coordinates) {
+                    return { x: 0, y: 0 }
+                }
+
+                const ellipsisRadius = ((this.coordinates.x1 - this.coordinates.x2) / 2) + parseInt(this.coordinates.x2)
+
+                return {
+                    x: ellipsisRadius,
+                    y: ellipsisRadius
+                }
+            }
+        },
+
+        methods: {
+            emitData() {
+                console.log('emited on hover')
+            }
         }
     })
 </script>
