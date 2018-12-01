710167f359c9edeba4d4a381bf758df840d716e9
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-12-01 14:57:03 +0700

    Add transition to text overlay

--- src/components/TextOverlay.vue
+++ src/components/TextOverlay.vue
@@ -1,19 +1,39 @@
 <template>
-    <ul class="list-reset absolute pin-b pin-l mb-8 ml-8">
-        <li v-for="(value, key) in source" class="mt-4">
-            <span class="text-blue-lighter text-xs uppercase">{{ key }}</span>
-            <p class="text-blue-lightest text-lg">{{ value }}</p>
-        </li>
-    </ul>
+    <transition name="slide-up">
+        <ul v-show="show" class="list-reset absolute pin-b pin-l mb-8 ml-8">
+            <li v-for="(value, key) in source" :key="key" class="mt-4">
+                <span class="text-blue-lighter text-xs uppercase">{{ key }}</span>
+                <p class="text-blue-lightest text-lg">{{ value }}</p>
+            </li>
+        </ul>
+    </transition>
 </template>
 
 <script>
+    import { isEmpty } from 'lodash'
+
     export default {
         props: {
             source: {
                 type: Object,
                 required: false,
             }
+        },
+
+        computed: {
+            show() {
+                return !isEmpty(this.source)
+            }
         }
     }
 </script>
+
+<style>
+    .slide-up-enter-active, .slide-up-leave-active {
+        transition: all 200ms;
+    }
+    .slide-up-enter, .slide-up-leave-to {
+        @apply opacity-0;
+        transform: translateY(-2rem);
+    }
+</style>
