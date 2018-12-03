764fb82de54c19c30c0e169b980a15577756a5a2
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-12-03 16:48:54 +0700

    Fix flicker when text overlay changed

--- src/components/TextOverlay.vue
+++ src/components/TextOverlay.vue
@@ -1,16 +1,17 @@
 <template>
     <ul class="list-reset absolute pin-b pin-l mb-8 ml-8">
-        <transition-step-in>
-            <li v-for="(value, key, index) in source" :style="`transition-delay: ${150 * index}ms`" :key="key" class="mt-4">
+        <transition-group @enter="enterTransition" @leave="leaveTransition">
+            <li v-for="(value, key, index) in source" :key="key + Date.now()" :data-index="index" class="mt-4">
                 <span class="text-blue-lighter text-xs uppercase">{{ key }}</span>
                 <p class="text-blue-lightest text-lg">{{ value }}</p>
             </li>
-        </transition-step-in>
+        </transition-group>
     </ul>
 </template>
 
 <script>
     import { isEmpty } from 'lodash'
+    import { TweenLite } from 'gsap'
 
     export default {
         props: {
@@ -18,16 +19,27 @@
                 type: Object,
                 required: false,
             }
+        },
+
+        methods: {
+            enterTransition(el, done) {
+                TweenLite.fromTo(el, 0.15, {
+                    x: -40,
+                    opacity: 0
+                }, {
+                    delay: el.dataset.index * 0.15,
+                    x: 0,
+                    opacity: 1,
+                    onComplete: done
+                })
+            },
+
+            leaveTransition(el, done) {
+                TweenLite.set(el, {
+                    opacity: 0,
+                    onComplete: done
+                })
+            }
         }
     }
 </script>
-
-<style>
-    .step-in-enter-active {
-        transition: all 150ms;
-    }
-    .step-in-enter, .step-in-leave-to {
-        @apply opacity-0;
-        transform: translateX(-2rem);
-    }
-</style>
