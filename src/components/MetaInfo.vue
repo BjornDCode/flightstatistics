ccf31a161ce1a36652fb0f3c0dcceb8f48ebe66e
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-12-03 18:04:04 +0700

    Add Sources modal

--- src/components/MetaInfo.vue
+++ src/components/MetaInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <nav>
-        <modal>
+        <modal title="About">
             <div slot="content">
                 <p class="leading-normal mb-4">
                     This website is primarily an experiment. I made it to play around with Vue and SVG's. To find out where the data comes from click on 'Sources' at the bottom of the page. Since the site is an experient it's not meant to be extremely strict or accurate about the data. The data has been chosen because it provided an interesting way to play around with the data visualisation. 
@@ -13,6 +13,28 @@
                         class="text-blue-lightest" 
                     >Twitter</a>.
                 </p>
+            </div>
+        </modal>
+        <modal title="Sources">
+            <div slot="content" >
+                <ul class="list-reset">
+                    <li class="leading-normal">
+                        <span>Most Profitable Routes: </span> 
+                        <a 
+                            href="https://www.oag.com/blog/billion-dollar-route" 
+                            target="_blank"
+                            class="text-blue-lightest" 
+                        >OAG</a>
+                    </li>
+                    <li class="leading-normal">
+                        <span>Busiest Routes: </span> 
+                        <a 
+                            href="https://www.routesonline.com/news/29/breaking-news/280568/these-are-the-top-100-busiest-routes-on-earth/" 
+                            target="_blank"
+                            class="text-blue-lightest"
+                        >Routes Online</a>
+                    </li>
+                </ul>
             </div>
         </modal>
     </nav>
