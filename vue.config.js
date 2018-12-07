bf13c34a2cc76c5f75a062add7e4199ebe527d34
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-12-07 15:21:31 +0700

    Setup purge css

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const path = require('path')
+const glob = require('glob-all')
+const PurgecssPlugin = require("purgecss-webpack-plugin")
+
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+}
+
 
 module.exports = {
     devServer: {
@@ -6,5 +15,20 @@ module.exports = {
         compress: true,
         port: 9000,
         host: '0.0.0.0',
+    },
+    configureWebpack: {
+        plugins: [
+            new PurgecssPlugin({
+                paths: glob.sync([
+                    path.join(__dirname, './**/*.vue'),
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+                        extensions: ["html", "js", "vue"]
+                    }
+                ]
+            })
+        ]
     }
 }
